190b9a63467d0bd45912b778d901f507e83649db
hael <haikaljawahir434@gmail.com>
2022-10-08 15:36:31 +0700

    first commit

---
+++ Assets/main.js
@@ -0,0 +1,40 @@
+
+const swiper = new Swiper(".mySwiper", {
+
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+        1000: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        }
+
+
+    }
+});
+
